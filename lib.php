e41f2d29ae9436e576bfe489f394de2aece767f4
Aleksei Kaminin <zendzirou@gmail.com>
2011-06-15 20:13:00 +0400

    yaml is now working

--- lib.php
+++ lib.php
@@ -1,8 +1,13 @@
 <?php
+require 'spyc.php';
 function unyaml($string){
 	return Spyc::YAMLLoadString($string);
 }
 
 function unyaml_file($file){
 	return unyaml(file_get_contents($file));
+}
+
+function tree($n){
+	return unyaml_file('trees/'.$n.'.yaml');
 }
---
+++ tests/core/is_yaml_working.php
@@ -0,0 +1,7 @@
+<?php
+$tree = tree(1);
+echo count($tree);
+echo "-";
+echo $tree['name'];
+---
+3-Anne
