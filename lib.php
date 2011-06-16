df48133aef35f54cb818ecadd9fc5f9dfe0d8d13
Aleksei Kaminin <zendzirou@gmail.com>
2011-06-16 22:30:28 +0400

    added more tests

--- lib.php
+++ lib.php
@@ -61,3 +61,41 @@ function p($wtf){
 function node(){
 	return new Node;
 }
+
+function calc_distance($tree, $dups){
+	$i = 0;
+
+	$first = array_shift($dups);
+	$second = array_shift($dups);
+	$p = $first->top;
+	while($p->self != $tree->self){
+		$i++;
+		$p = $p->top;
+	}
+
+	$p = $second->top;
+	while($p->self != $tree->self){
+		$i++;
+		$p = $p->top;
+	}
+
+	return $i;
+}
+
+function nearest_common_node(){
+
+}
+
+function calc_imbriding($tree){
+	$dups = find_dups($tree);
+	//	foreach(
+
+	//$k = (0.5 * pow(0.5, calc_distance(nearest_common_node($tree, array_shift($dups))))) * 100;
+
+	return array(array('node'=>$tree,
+			   'num'=>$k));
+}
+function print_imbriding($data){
+	foreach($data as $v)
+		echo $v['node']->self." - ".$v['num']."\n";
+}
